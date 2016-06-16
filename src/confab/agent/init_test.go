c39faaa93175430b82177ee7b8053ae7b0cb2678
Charlie Vieth <cviethjr@pivotal.io>
2016-06-16 15:17:06 -0400

    Enable testing on Windows
    * Windows and Unix have different error text for the same error codes
    
    [#121247095]
    
    Signed-off-by: Brandon Shroyer <bshroyer@pivotal.io>
    Signed-off-by: Charlie Vieth <cviethjr@pivotal.io>

--- src/confab/agent/init_test.go
+++ src/confab/agent/init_test.go
@@ -4,11 +4,9 @@ import (
 	"bytes"
 	"encoding/json"
 	"io/ioutil"
-	"os"
 	"path/filepath"
 	"strconv"
 	"sync"
-	"syscall"
 	"testing"
 
 	"github.com/cloudfoundry-incubator/consul-release/src/confab/agent"
@@ -66,18 +64,6 @@ func getPID(runner *agent.Runner) (int, error) {
 	}
 
 	return pid, nil
-}
-
-func processIsRunning(runner *agent.Runner) bool {
-	pid, err := getPID(runner)
-	Expect(err).NotTo(HaveOccurred())
-
-	process, err := os.FindProcess(pid)
-	Expect(err).NotTo(HaveOccurred())
-
-	errorSendingSignal := process.Signal(syscall.Signal(0))
-
-	return (errorSendingSignal == nil)
 }
 
 type concurrentSafeBuffer struct {
--- src/confab/confab/confab_test.go
+++ src/confab/confab/confab_test.go
@@ -14,7 +14,8 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-const COMMAND_TIMEOUT = "15s"
+const COMMAND_TIMEOUT = time.Second * 15
+const POLL_INTERVAL = time.Millisecond * 250
 
 var _ = Describe("confab", func() {
 	var (
@@ -34,6 +35,8 @@ var _ = Describe("confab", func() {
 
 		pidFile, err = ioutil.TempFile(tempDir, "fake-pid-file")
 		Expect(err).NotTo(HaveOccurred())
+
+		Expect(pidFile.Close()).To(Succeed())
 
 		err = os.Remove(pidFile.Name())
 		Expect(err).NotTo(HaveOccurred())
@@ -125,8 +128,9 @@ var _ = Describe("confab", func() {
 			)
 			Eventually(stop.Run, COMMAND_TIMEOUT, COMMAND_TIMEOUT).Should(Succeed())
 
-			_, err = isPIDRunning(pid)
-			Expect(err).To(MatchError(ContainSubstring("process already finished")))
+			Eventually(func() bool {
+				return isPIDRunning(pid)
+			}, COMMAND_TIMEOUT, time.Millisecond*250).Should(BeFalse())
 
 			Expect(fakeAgentOutput(consulConfigDir)).To(Equal(FakeAgentOutputData{
 				PID: pid,
@@ -311,6 +315,12 @@ var _ = Describe("confab", func() {
 			})
 
 			It("checks sync state up to the timeout", func() {
+				if Windows {
+					// On Windows the timeout works, but due to the
+					// overhead of shelling out the elapsed time
+					// regularly exceeds the 1 second tolerance.
+					Skip("Flaky on Windows due to the overhead of shelling out")
+				}
 				writeConfigurationFile(configFile.Name(), map[string]interface{}{
 					"path": map[string]interface{}{
 						"agent_path":        pathToFakeAgent,
@@ -380,6 +390,7 @@ var _ = Describe("confab", func() {
 				"--config-file", configFile.Name(),
 			)
 			Eventually(cmd.Run, COMMAND_TIMEOUT, COMMAND_TIMEOUT).Should(Succeed())
+
 			Eventually(func() error {
 				conn, err := net.Dial("tcp", "localhost:8400")
 				if err == nil {
@@ -401,7 +412,9 @@ var _ = Describe("confab", func() {
 				return pidIsForRunningProcess(pidFile.Name())
 			}, "5s").Should(BeFalse())
 
-			Expect(fakeAgentOutput(consulConfigDir)).To(Equal(FakeAgentOutputData{
+			Eventually(func() (FakeAgentOutputData, error) {
+				return fakeAgentOutput(consulConfigDir)
+			}, COMMAND_TIMEOUT, POLL_INTERVAL).Should(Equal(FakeAgentOutputData{
 				PID: pid,
 				Args: []string{
 					"agent",
@@ -651,7 +664,10 @@ var _ = Describe("confab", func() {
 				cmd.Stderr = buffer
 				Eventually(cmd.Run, COMMAND_TIMEOUT, COMMAND_TIMEOUT).ShouldNot(Succeed())
 				Expect(buffer).To(ContainSubstring("error during start"))
-				Expect(buffer).To(ContainSubstring("connection refused"))
+				Expect(buffer).To(Or(
+					ContainSubstring("connection refused"),
+					ContainSubstring("No connection could be made"), // Windows
+				))
 			})
 		})
 
@@ -676,7 +692,10 @@ var _ = Describe("confab", func() {
 				buffer := bytes.NewBuffer([]byte{})
 				cmd.Stderr = buffer
 				Eventually(cmd.Run, COMMAND_TIMEOUT, COMMAND_TIMEOUT).ShouldNot(Succeed())
-				Expect(buffer).To(ContainSubstring("no such file or directory"))
+				Expect(buffer).To(Or(
+					ContainSubstring("no such file or directory"),
+					ContainSubstring("The system cannot find the file specified"), // Windows
+				))
 			})
 		})
 
@@ -684,6 +703,7 @@ var _ = Describe("confab", func() {
 			It("returns an error and exits with status 1", func() {
 				tmpFile, err := ioutil.TempFile(tempDir, "config")
 				Expect(err).NotTo(HaveOccurred())
+				defer tmpFile.Close()
 
 				_, err = tmpFile.Write([]byte(`%%%%%%%%%`))
 				Expect(err).NotTo(HaveOccurred())
@@ -728,6 +748,9 @@ var _ = Describe("confab", func() {
 			})
 
 			It("returns an error and exits with status 1", func() {
+				if Windows {
+					Skip("Not implemented on Windows")
+				}
 				err := os.Chmod(consulConfigDir, 0000)
 				Expect(err).NotTo(HaveOccurred())
 
